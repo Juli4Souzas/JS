421a883eb8f58f47a968291f31f1855789711446
Julia Santos <souzasjulia14@gmail.com>
2024-08-30 18:12:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//Troca o conteúdo do lula por esquerda
+//document.getElementById('esquerda').innerHTML = "Conteúdo da esquerda";
+
+/*Troca o conteúdo do
+Bolsonaro por direita*/
+//document.getElementById('direita').innerHTML = "Conteúdo da direita";
+
+//Variáveis
+let nome = "Ale";
+var idade = 45;
+
+const cpf = "000.000.000-00";
+
+let saldo = 2;
+let compra = 2;
+let deposito = 100;
+
+//window.alert(nome + " tem " +  idade + " possui na conta " + saldo + " fez compra no valor de " + compra);
+
+//saldo = saldo - compra;
+//saldo = saldo + depósito;
+
+saldo **= compra;
+//sando += deposito;
+
+window.alert("seja muito bem-vindo");
+console.log('Desenvolvedor (a) : Julia Santos de Souza');
